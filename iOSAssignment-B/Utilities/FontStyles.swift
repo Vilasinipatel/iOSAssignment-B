dfa51812fa7c0eab097873057807a41e53bffc8f
Villasini Patel <villasinipatel@villasinis-macbook-pro.local>
2018-07-16 17:22:41 +0530

    Assignment version changes.

---
+++ iOSAssignment-B/Utilities/FontStyles.swift
@@ -0,0 +1,11 @@
+// FontStyles.swift
+
+import UIKit
+extension UIFont {
+    static var descFont:UIFont {
+        return UIFont(name: "Helvetica", size: 15)!
+    }
+    static var titleFont:UIFont {
+        return UIFont(name: "Helvetica-Bold", size: 15)!
+    }
+}
---
+++ iOSAssignment-BTests/Utilities/Constants.swift
@@ -0,0 +1,23 @@
+//
+//  Constants.swift
+//  iOSAssignment-B
+//
+//  Created by Villasini Patel on 14/07/18.
+//  Copyright © 2018 vils. All rights reserved.
+//
+
+import Foundation
+class Constants {
+    // MARK: List of Constants
+    static let jsonUrl = "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json"
+    static let imageUrl = "http://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/American_Beaver.jpg/220px-American_Beaver.jpg"
+    static let imagePlaceHolder = "PlaceHolder"
+    static let networkErrorMessage = "Make sure your device is connected to the internet."
+    static let networkErrorMessageTitle = "No Internet Connection"
+    static let collectionViewReusableCellId = "collectionResuableCell"
+    static let okOptionText = "Ok"
+    static let titleKey = "title"
+    static let itemDescKey = "description"
+    static let itemImageRef = "imageHref"
+    static let rowsContentKey = "rows"
+}
